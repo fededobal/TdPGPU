2e1cf61177936d4d7b8ecd43cb5f15858c6800f9
Federico Dobal <fededobal@MacBook-Air-de-Federico.local>
2026-09-04 11:02:05 -0300

    vault backup: 2026-09-04 11:02:05

--- Prácticas/TP1.cu
+++ Prácticas/TP1.cu
@@ -1,6 +1,8 @@
+%%writefile TP1.cu
 #include <cuda.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
 
 __global__ void punto1a(int C, int N, int *d_V) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -9,8 +11,17 @@ __global__ void punto1a(int C, int N, int *d_V) {
     }
 }
 
+double dwalltime(){
+        double sec;
+        struct timeval tv;
+
+        gettimeofday(&tv,NULL);
+        sec = tv.tv_sec + tv.tv_usec/1000000.0;
+        return sec;
+}
+
 int main(int argc, char** argv) {
-    int N = 50;
+    int N = 1000000000;
     int threadsXBloque = 256;
     int bloques = (N + threadsXBloque - 1) / threadsXBloque;
 
@@ -24,17 +35,12 @@ int main(int argc, char** argv) {
     cudaMalloc((void **) &d_V, bytes);
     cudaMemcpy(d_V, h_V, bytes, cudaMemcpyHostToDevice);
 
-    for (int i = 0; i < N; i++) {
-        printf("%d ", h_V[i]);
-    }
-    printf("\n");
-
+    double ini = dwalltime();
     punto1a<<<bloques, threadsXBloque>>>(10, N, d_V);
+    cudaDeviceSynchronize();
+    double fin = dwalltime();
 
     cudaMemcpy(h_V, d_V, bytes, cudaMemcpyDeviceToHost);
 
-    for (int i = 0; i < N; i++) {
-        printf("%d ", h_V[i]);
-    }
-    printf("\n");
+    printf("%f", fin - ini);
 }
